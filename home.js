15e9b3250261ad55c72e89a7017f38a4e89f9e1f
unknown <thilakeswarandg2007@gmail.com>
2025-12-25 00:38:00 +0530

    Commit Changes

---
+++ home.js
@@ -0,0 +1,8 @@
+var navmenu = document.getElementById("nav-menu");
+var menubutton = document.querySelector(".menu-btn");
+
+menubutton.addEventListener("click",function(){
+    navmenu.classList.toggle("active");
+    navmenu.style.back
+})
+
